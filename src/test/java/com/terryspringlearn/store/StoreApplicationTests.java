f43a068fa196c422903dd12215bb0cbfa6e6b8fb
Chongda You <74991409+chongdayou@users.noreply.github.com>
2025-06-19 15:29:12 -0500

    Initial commit to repository. Added initial Maven dependencies.

---
+++ src/test/java/com/terryspringlearn/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terryspringlearn.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
